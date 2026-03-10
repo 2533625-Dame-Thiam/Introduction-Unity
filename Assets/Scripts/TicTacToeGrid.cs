08444a60bf2073521a1315c8f0391c602019ea52
Dame Thiam <2533625@etu.cchic.ca>
2026-03-10 10:07:49 -0400

    modification

--- Assets/Scripts/TicTacToeGrid.cs
+++ Assets/Scripts/TicTacToeGrid.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 using Random = System.Random;
@@ -62,6 +64,73 @@ public class TicTacToeGrid : MonoBehaviour
         //TODO Vous devrez programmer la vérification de victoire ici
         //S'il y a un gagnant, appeller la fonction ShowWinner(turn)
         //En cas d'égalité, appeller la fonction ShowWinner et faite en sorte que celle-ci gère les partie nulle
+
+        for (int i = 0; i < tictactoeGrid.Length; i++)
+        {
+            //for (int j = 0; j<3; j++)
+            {
+                if (tictactoeGrid[0, 0]=='X'&& tictactoeGrid[0, 1]=='X'&& tictactoeGrid[0, 2]=='X' || tictactoeGrid[0, 0]=='O'&& tictactoeGrid[0, 1]=='O'&& tictactoeGrid[0, 2]=='O')
+                {
+                    ShowWinner(turn);
+                }
+            }
+            //for (int j = 1; j<4; j++)
+            {
+                if (tictactoeGrid[1, 0]=='X'&& tictactoeGrid[1, 1]=='X'&& tictactoeGrid[1, 2]=='X' || tictactoeGrid[1, 0]=='O'&& tictactoeGrid[1, 1]=='O'&& tictactoeGrid[1, 2]=='O')
+                {
+                    ShowWinner(turn);
+                }
+                
+            }
+            //for (int j = 2; j<5; j++)
+            {
+                if (tictactoeGrid[2, 0]=='X'&& tictactoeGrid[2, 1]=='X'&& tictactoeGrid[2, 2]=='X' || tictactoeGrid[2, 0]=='O'&& tictactoeGrid[2, 1]=='O'&& tictactoeGrid[2, 2]=='O')
+                {
+                    ShowWinner(turn);
+                }
+                
+            }
+
+            for (int j = 0; j<7; j++)
+            {
+                if (tictactoeGrid[0, 0]=='X'&& tictactoeGrid[1, 0]=='X'&& tictactoeGrid[2, 0]=='X' || tictactoeGrid[0, 0]=='O'&& tictactoeGrid[1, 0]=='O'&& tictactoeGrid[2, 0]=='O')
+                {
+                    ShowWinner(turn);
+                }
+            }
+            //for (int j = 0; j<9; j++)
+            {
+                if (tictactoeGrid[0, 1]=='X'&& tictactoeGrid[1, 1]=='X'&& tictactoeGrid[2, 1]=='X' || tictactoeGrid[0, 1]=='O'&& tictactoeGrid[1, 1]=='O'&& tictactoeGrid[2, 1]=='O')
+                {
+                    ShowWinner(turn);
+                }
+            }
+            //for (int j = 0; j<10; j++)
+            {
+                if (tictactoeGrid[0, 2]=='X'&& tictactoeGrid[1, 2]=='X'&& tictactoeGrid[2, 2]=='X' || tictactoeGrid[0, 2]=='O'&& tictactoeGrid[1, 2]=='O'&& tictactoeGrid[2, 2]=='O')
+                {
+                    ShowWinner(turn);
+                }
+            }
+            //for (int j = 0; j<10; j++)
+            {
+                if (tictactoeGrid[0, 0]=='X'&& tictactoeGrid[1, 1]=='X'&& tictactoeGrid[2, 2]=='X' || tictactoeGrid[0, 0]=='O'&& tictactoeGrid[1, 1]=='O'&& tictactoeGrid[2, 2]=='O')
+                {
+                    ShowWinner(turn);
+                }
+            }
+            //for (int j = 0; j<8; j++)
+            {
+                if (tictactoeGrid[0, 2]=='X'&& tictactoeGrid[1, 1]=='X'&& tictactoeGrid[2, 0]=='X' || tictactoeGrid[0, 2]=='O'&& tictactoeGrid[1, 1]=='O'&& tictactoeGrid[2, 0]=='O')
+                {
+                    ShowWinner(turn);
+                }
+            }
+          
+
+            
+
+        }
     }
 
     public void PlayRandomTurn()
@@ -84,8 +153,36 @@ public class TicTacToeGrid : MonoBehaviour
     public void ShowWinner(char winner)
     {
         //TODO Vous aurez à modifier légèrement cette fonction afin de faire fonctionner les partie nulle
-        winnerPanel.SetActive(true);
-        winnerText.text = "Le gagnant est : " + winner;
+       
+        for (int i = 0; i<tictactoeGrid.Length; i++)
+        {
+           
+            if (i<tictactoeGrid.Length)
+            {
+                winnerPanel.SetActive(true);
+                winnerText.text = "Le gagnant est : " + winner;
+
+            }
+            else
+            {
+                for (int y = 0; y<tictactoeGrid.Length; i++) 
+                {
+                    if (symbols[i,y].sprite=xSymbol)
+                        symbols[i,y].sprite = null;
+                    else
+                        symbols[i,y].sprite =null;
+                    
+                       
+                }
+                   
+
+            }
+        }
+             
+        
+            
+        
+       
     }
 
 
